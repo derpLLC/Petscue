80543a963463bac954d497d84ff92033c0dcd491
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-27 12:15:09 +0530

    lib: main: use routes
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'screen/login.dart';
+import 'screen/homescreen.dart';
+import 'utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      initialRoute: MyRoutes.loginRoute,
+      routes: {
+        "/": (context) => Login(),
+        MyRoutes.homeRoute: (context)  => HomeScreen(),
+        MyRoutes.loginRoute: (context)=> Login(),
+      },
     );
   }
 }
