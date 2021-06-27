6d1748593d66fff7276cadaa2e65b92bbb5b005b
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-27 12:33:51 +0530

    lib: login: add text field for password and username
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'homescreen.dart';
-
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,23 +9,39 @@ class Login extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            Image.asset(
-              "assets/images/login_screen_image.jpg",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Welcome",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24.0,
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
+            Padding(
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 36),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/images/login_screen_image.jpg",
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text(
+                      "Welcome",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Enter the username',
+                          labelText: 'Username'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Enter the password',
+                          labelText: 'Password'),
+                    )
+                  ],
+                )),
           ],
         ),
       ),
