3255bf142f235405c09d3c1a0a0ea8e670bd57e3
ramanverma2k <ramanverma4183@gmail.com>
2021-06-27 11:37:26 +0530

    *finish coding homescreen ui*
    Some minor changes are needed.
    Font Awesome Icons aren't showing up, dunno whoi ;_;.

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -1,118 +1,257 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        margin: EdgeInsets.only(top: 40, left: 10, right: 10),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                MaterialButton(
-                  onPressed: () => {
-                    print('settings'),
-                  },
-                  minWidth: 10,
-                  child: Icon(Icons.menu),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(top: 40, left: 10, right: 10),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MaterialButton(
+                    onPressed: () => {
+                      print('settings'),
+                    },
+                    minWidth: 10,
+                    child: Icon(Icons.menu),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => {print("Profile")},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Weasel',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://images.pexels.com/photos/1629781/pexels-photo-1629781.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+                                fit: BoxFit.fill),
+                          ),
+                        )
+                      ],
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        elevation: 5,
+                        shadowColor: Colors.grey,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        primary: Colors.white),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: size.height * 0.25,
+                width: size.width * 1,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
                   color: Colors.grey,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        'https://images.pexels.com/photos/4422100/pexels-photo-4422100.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
                   ),
                 ),
-                MaterialButton(
-                  onPressed: () => {
-                    print('settings'),
-                  },
-                  color: Colors.grey,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Row(
+                child: Container(
+                  // color: Colors.red,
+                  margin: EdgeInsets.only(left: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('profile'),
-                      SizedBox(
-                        width: 10,
+                      Text(
+                        '50% off',
+                        style: TextStyle(color: Colors.pink),
                       ),
-                      Icon(Icons.person),
+                      Text(
+                        'Fresh sell',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      MaterialButton(
+                        onPressed: () => {
+                          print('see all'),
+                        },
+                        child: Text(
+                          'See all',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.pink,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
                     ],
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.25,
-              width: MediaQuery.of(context).size.width * 0.9,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.grey,
               ),
-              child: Container(
-                margin: EdgeInsets.only(left: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              SizedBox(height: 20),
+              Material(
+                elevation: 5,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: size.height * 0.065,
+                  padding: EdgeInsets.only(left: 20),
+                  child: TextField(
+                    textAlignVertical: TextAlignVertical.center,
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      hintStyle: TextStyle(color: Colors.black26),
+                      suffixIcon: FaIcon(FontAwesomeIcons.search),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      '50% off',
-                      style: TextStyle(color: Colors.pink),
-                    ),
-                    Text(
-                      'Fresh sell',
+                      'Explore Popular',
                       style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
                     ),
-                    MaterialButton(
-                      onPressed: () => {
-                        print('see all'),
-                      },
-                      child: Text(
-                        'See all',
-                        style: TextStyle(color: Colors.white),
-                      ),
+                    Icon(
+                      Icons.arrow_forward,
                       color: Colors.pink,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
+                      size: 30,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: size.height * 0.4,
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Card(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://images.pexels.com/photos/4588435/pexels-photo-4588435.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                            ),
+                            borderRadius: BorderRadius.circular(20)),
+                        width: size.width * 0.7,
+                        child: Container(
+                          padding:
+                              EdgeInsets.only(top: 20, left: 20, right: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Lucy',
+                                      style: TextStyle(
+                                          fontSize: 25,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      '799\$',
+                                      style: TextStyle(
+                                          fontSize: 20, color: Colors.white),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () => {},
+                                icon: FaIcon(FontAwesomeIcons.heart),
+                                color: Colors.white,
+                                iconSize: 30,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Card(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://images.pexels.com/photos/4588435/pexels-photo-4588435.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                            ),
+                            borderRadius: BorderRadius.circular(20)),
+                        width: size.width * 0.7,
+                        child: Container(
+                          padding:
+                              EdgeInsets.only(top: 20, left: 20, right: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Lucy',
+                                      style: TextStyle(
+                                          fontSize: 25,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      '799\$',
+                                      style: TextStyle(
+                                          fontSize: 20, color: Colors.white),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () => {},
+                                icon: FaIcon(FontAwesomeIcons.heart),
+                                color: Colors.white,
+                                iconSize: 30,
+                              )
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              height: 60,
-              width: MediaQuery.of(context).size.width * 0.9,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextField(),
-            ),
-            SizedBox(height: 20),
-            Container(
-              margin: EdgeInsets.only(left: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Explore Popular',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                  ),
-                  Icon(
-                    Icons.arrow_forward,
-                    color: Colors.pink,
-                    size: 30,
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
