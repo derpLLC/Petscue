23034ee215f1e04d7202bfa1e0101d3e0561b461
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-27 13:21:20 +0530

    lib: main: set theme according to system
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
       theme: ThemeData(
         fontFamily: GoogleFonts.montserrat().fontFamily,
       ),
