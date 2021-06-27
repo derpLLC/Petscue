3fba5eb846c3b73d1abb552f46f81f5d6e1e841a
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-27 13:01:42 +0530

    lib: login: implement login button
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -10,39 +10,46 @@ class Login extends StatelessWidget {
         child: Column(
           children: [
             Padding(
-                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 36),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "assets/images/login.png",
-                      fit: BoxFit.cover,
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 36),
+              child: Column(
+                children: [
+                  Image.asset(
+                    "assets/images/login.png",
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Text(
-                      "Welcome",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24.0,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                          hintText: 'Enter the username',
-                          labelText: 'Username'),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: 'Enter the password',
-                          labelText: 'Password'),
-                    )
-                  ],
-                )),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter the username', labelText: 'Username'),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: 'Enter the password', labelText: 'Password'),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
