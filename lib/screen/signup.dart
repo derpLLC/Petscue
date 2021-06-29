ed219609959cb77c7a773c5196c5cb9bc2a07b6a
ramanverma2k <ramanverma4183@gmail.com>
2021-06-29 20:34:07 +0530

    singup: add signup logic and validation

--- lib/screen/signup.dart
+++ lib/screen/signup.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:petshop/utils/fire_auth.dart';
 import 'package:petshop/utils/routes.dart';
+import 'package:provider/provider.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -9,6 +11,12 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String? _usernameError;
+  String? _passwordError;
+
   bool changeButton = false;
 
   @override
@@ -37,19 +45,23 @@ class _SignUpState extends State<SignUp> {
                 height: 20.0,
               ),
               TextFormField(
+                controller: _emailController,
                 decoration: InputDecoration(
                   hintText: 'Enter the username',
                   labelText: 'Username',
+                  errorText: _usernameError,
                 ),
               ),
               SizedBox(
                 height: 20.0,
               ),
               TextFormField(
+                controller: _passwordController,
                 obscureText: true,
                 decoration: InputDecoration(
                   hintText: 'Enter the password',
                   labelText: 'Password',
+                  errorText: _passwordError,
                 ),
               ),
               SizedBox(
@@ -60,8 +72,36 @@ class _SignUpState extends State<SignUp> {
                   setState(() {
                     changeButton = true;
                   });
-                  await Future.delayed(Duration(seconds: 1));
-                  await Navigator.pushNamed(context, MyRoutes.loginRoute);
+
+                  if (_emailController.text.isEmpty &&
+                      _passwordController.text.isEmpty) {
+                    setState(() {
+                      _usernameError = 'Username cannot be empty.';
+                      _passwordError = 'Password cannot be empty';
+                    });
+                  } else if (_emailController.text.isEmpty) {
+                    setState(() {
+                      _usernameError = 'Username cannot be empty.';
+                    });
+                  } else if (_passwordController.text.isEmpty) {
+                    setState(() {
+                      _passwordError = 'Password cannot be empty.';
+                    });
+                  } else {
+                    setState(() {
+                      _usernameError = '';
+                      _passwordError = '';
+                    });
+                    context.read<FireAuth>().signUp(
+                        email: _emailController.text.trim(),
+                        password: _passwordController.text.trim());
+
+                    await Future.delayed(Duration(seconds: 1));
+                    await Navigator.pushNamed(context, MyRoutes.loginRoute);
+                    setState(() {
+                      changeButton = false;
+                    });
+                  }
                   setState(() {
                     changeButton = false;
                   });
@@ -86,7 +126,8 @@ class _SignUpState extends State<SignUp> {
                         ),
                   decoration: BoxDecoration(
                       color: Colors.pink,
-                      borderRadius: BorderRadius.circular(changeButton? 50 : 8.0)),
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8.0)),
                 ),
               ),
             ],
