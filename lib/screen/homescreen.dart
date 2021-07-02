5eb71b9f0bcb930f2aa588f9ac7b3d1fe5384061
ramanverma2k <ramanverma4183@gmail.com>
2021-07-02 23:47:29 +0530

    homescreen: implement side drawer * Use Builder widget to get the context of Scaffold * Scaffold context is needed in order to open the drawer using a button.

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -26,163 +26,199 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.only(top: 40, left: 10, right: 10),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  MaterialButton(
-                    onPressed: () => {
-                      print('settings'),
-                    },
-                    minWidth: 10,
-                    child: Icon(Icons.menu),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  Material(
-                    elevation: 20,
-                    borderRadius: BorderRadius.circular(30),
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(30),
-                      onTap: () {
-                        if (_user == 'Sign in') {
-                          Navigator.pushNamed(context, MyRoutes.loginRoute);
-                        } else {
-                          context.read<FireAuth>().signOut();
-                        }
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(left: 10),
-                            child: Text(
-                              '$_user',
-                              style: TextStyle(color: Colors.black),
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                          SizedBox(width: 10),
-                          Container(
-                            height: 35,
-                            width: 35,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.grey,
-                              image: DecorationImage(
-                                  image: NetworkImage(
-                                      'https://images.pexels.com/photos/1629781/pexels-photo-1629781.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                                  fit: BoxFit.cover),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: 20),
-              Container(
-                height: size.height * 0.25,
-                width: size.width * 1,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey,
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                        'https://images.pexels.com/photos/4422100/pexels-photo-4422100.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                  ),
-                ),
-                child: Container(
-                  // color: Colors.red,
-                  margin: EdgeInsets.only(left: 20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '50% off',
-                        style: TextStyle(color: Colors.pink),
-                      ),
-                      Text(
-                        'Fresh sell',
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
-                      ),
-                      MaterialButton(
-                        onPressed: () => {
-                          print('see all'),
-                        },
-                        child: Text(
-                          'See all',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        color: Colors.pink,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(40),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Material(
-                elevation: 5,
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  height: size.height * 0.065,
-                  padding: EdgeInsets.only(left: 20, right: 10),
-                  child: TextField(
-                    textAlignVertical: TextAlignVertical.center,
-                    decoration: InputDecoration(
-                      hintText: 'Search',
-                      hintStyle: TextStyle(color: Colors.black26),
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: FaIcon(FontAwesomeIcons.search),
-                      ),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                child: Row(
+      body: Builder(
+        builder: (context) => SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(top: 40, left: 10, right: 10),
+            child: Column(
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      'Explore Popular',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+                    MaterialButton(
+                      onPressed: () => {
+                        Scaffold.of(context).openDrawer(),
+                      },
+                      minWidth: 10,
+                      child: Icon(Icons.menu),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
                     ),
-                    Icon(
-                      Icons.arrow_forward,
-                      color: Colors.pink,
-                      size: 30,
+                    Material(
+                      elevation: 20,
+                      borderRadius: BorderRadius.circular(30),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(30),
+                        onTap: () {
+                          if (_user == 'Sign in') {
+                            Navigator.pushNamed(context, MyRoutes.loginRoute);
+                          } else {
+                            context.read<FireAuth>().signOut();
+                          }
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(left: 10),
+                              child: Text(
+                                '$_user',
+                                style: TextStyle(color: Colors.black),
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Container(
+                              height: 35,
+                              width: 35,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey,
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        'https://images.pexels.com/photos/1629781/pexels-photo-1629781.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+                                    fit: BoxFit.cover),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     )
                   ],
                 ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                height: size.height * 0.4,
-                child: ListView.builder(
-                  itemBuilder: _cardsWidget,
-                  itemCount: 2,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
+                SizedBox(height: 20),
+                Container(
+                  height: size.height * 0.25,
+                  width: size.width * 1,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.grey,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          'https://images.pexels.com/photos/4422100/pexels-photo-4422100.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+                    ),
+                  ),
+                  child: Container(
+                    // color: Colors.red,
+                    margin: EdgeInsets.only(left: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '50% off',
+                          style: TextStyle(color: Colors.pink),
+                        ),
+                        Text(
+                          'Fresh sell',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        MaterialButton(
+                          onPressed: () => {
+                            print('see all'),
+                          },
+                          child: Text(
+                            'See all',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.pink,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              )
-            ],
+                SizedBox(height: 20),
+                Material(
+                  elevation: 5,
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    height: size.height * 0.065,
+                    padding: EdgeInsets.only(left: 20, right: 10),
+                    child: TextField(
+                      textAlignVertical: TextAlignVertical.center,
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        hintStyle: TextStyle(color: Colors.black26),
+                        suffixIcon: IconButton(
+                          onPressed: () {},
+                          icon: FaIcon(FontAwesomeIcons.search),
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Explore Popular',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 28),
+                      ),
+                      Icon(
+                        Icons.arrow_forward,
+                        color: Colors.pink,
+                        size: 30,
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  height: size.height * 0.4,
+                  child: ListView.builder(
+                    itemBuilder: _cardsWidget,
+                    itemCount: 2,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                )
+              ],
+            ),
           ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text('Home'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Categories'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Liked'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Orders'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Your Listings'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('About Us'),
+              onTap: () {},
+            ),
+          ],
         ),
       ),
     );
@@ -238,9 +274,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      //   SizedBox(
-      //     width: 30,
-      //   ),
     );
   }
 }
